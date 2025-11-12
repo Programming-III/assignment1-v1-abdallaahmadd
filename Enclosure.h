56751dc3268fa17ddf2cf2edd30fc253bbb9e0f9
abdallaahmadd <abdallahahmadd88@gmail.com>
2025-11-12 15:23:37 +0200

    Refactor Enclosure class in Enclosure.h
    Fix constructor and destructor syntax, and add animal handling methods.

--- Enclosure.h
+++ Enclosure.h
@@ -3,8 +3,33 @@
 
 #include "Animal.h"
 
-//define enclosure class here 
-
+class Enclosure{
+    private:
+    Animal* Animal; 
+    int capacity;
+    int currentCount;
+public:
+Enclosure(){
+    capacity=30;
+    currentCount=0'
+}
+Enclosure(Animal* Animal, int Capacity, int currentCount){
+    this->Animal=Animal;
+    this->Capacity=Capacity;
+    this->currentCount=currentCount;
+}
+~Enclosure{
+    delete [];
+}
+void addAnimal(Animal* a){
+    If(capacity<currentCount){
+    currentCount++;
+    }
+}
+void display (){
+    cout<<"capacity is"<<capacity<<"count is"<<currentCount<<endl;
+}
+};
 
 
 
