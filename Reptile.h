344ac62f41f7bb1283ee78cd50b5d9b4dd34e88d
abdallaahmadd <abdallahahmadd88@gmail.com>
2025-11-12 15:24:46 +0200

    Update Reptile.h

--- Reptile.h
+++ Reptile.h
@@ -5,6 +5,22 @@
 
 //define reptile class here
 
+class Reptile : Animal(){
+    private:
+    bool isVenomous;
+public:
+Reptile() : Animal(){
+    isVenomous=false; 
+}
+Reptile(bool isVenomous) : Animal(string name, int age, bool isHungry) {
+    this->isVenomous=isVenomous;
+}
+~Reptile{}
+void display(){
+    cout<<"is it venomus:"<<isVenomous<<endl;
+}
+
+};
 
 
 
