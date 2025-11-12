525e071e041f0a5aaae61c0a978ff2fc7dbd16bb
abdallaahmadd <abdallahahmadd88@gmail.com>
2025-11-12 15:20:44 +0200

    Implement Animal class with basic functionality
    Added Animal class with constructors, destructor, and methods.

--- Animal.h
+++ Animal.h
@@ -5,7 +5,46 @@
 using namespace std;
 
 //define animal class here 
+class Animal {
+private:
+    string name;
+    int age;
+    bool isHungry;
 
+public:
+    
+    Animal() {
+        name = "unknown";
+        age = 0;
+        isHungry = false;
+    }
 
+    Animal(string name, int age, bool isHungry) {
+        this->name = name;
+        this->age = age;
+        this->isHungry = isHungry;
+    }
 
+   
+    ~Animal() {
+        
+    }
+
+   virtual void display() {
+        cout <<"name is:"<<name<<"age is"<<age<<"hungry?"<<isHungry<<endl
+    }
+
+    // Feed function
+    void feed() {
+        if (isHungry) {
+            cout << name << " is hungry" << endl;
+            isHungry = false;
+        } else {
+            cout << name << " is not hungry" << endl;
+        }
+    }
+};
 #endif
+
+
+
