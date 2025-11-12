38bff3b2582dbe76afcc1e098e23e4b8035d944b
abdallaahmadd <abdallahahmadd88@gmail.com>
2025-11-12 15:25:19 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -5,7 +5,24 @@
 using namespace std;
 
 //define visitor class here
-
+class visiotr{
+    private:
+    string VisiorName;
+    int ticketBought; 
+public:
+visitor(){
+    VisiorName="ahmed";
+    ticketBought="0";
+}
+visitor(string VisiorName, int ticketBought){
+    this->VisiorName=VisiorName;
+    this->ticketBought=ticketBought;
+}
+~visitor{}
+void display(){
+    cout<<"name"<<VisiorName<<"tickets bought:"<<ticketBought<<endl;
+}
+};
 
 
 
